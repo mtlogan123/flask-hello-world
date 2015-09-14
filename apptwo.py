8d925e2cd84c904f6aae2fe89833f389346406db
TROY LOGAN <tlogan@warrenfinancial.com>
2015-09-14 16:59:50 -0400

    flask-hello-world

--- apptwo.py
+++ apptwo.py
@@ -7,12 +7,23 @@ from flask import Flask
 app = Flask(__name__)
 
 # use decorators to link the function to an url
-@app.route("/")
-@app.route("/hello")
+#@app.route("/")
+#@app.route("/hello")
 #@app.route("/test/<search_query>")
-
-#def search(search_query):
-#	return search_query
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value +1
+	return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+def search(search_query):
+	return search_query
 
 # define the view using a function, which returns a string
 def hello_world():
